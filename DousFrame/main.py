dc4aa0a7b50949f2cac51ae67fb4e5a3eb4a478c
pavel <pasha050598@mail.ru>
2022-10-25 21:39:26 +0300

    part2

--- DousFrame/main.py
+++ DousFrame/main.py
@@ -1,3 +1,7 @@
+from DousFrame.myRequests import PostRequest, GetRequest
+from quopri import decodestring
+
+
 class PageNotFound404:
     def __call__(self, request):
         return '404', '404 - ERROR\n  There are not such page!'
@@ -18,6 +22,23 @@ class DousFrame:
             view = PageNotFound404()
 
         request = {}
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequest().get_request(environ)
+            request['data'] = DousFrame.decode_value(data)
+            print(f'Нам пришёл post-запрос: {DousFrame.decode_value(data)}')
+        if method == 'GET':
+            request_params = GetRequest().get_request(environ)
+            request['request_params'] = DousFrame.decode_value(request_params)
+            print(f'Нам пришли GET-параметры:'
+                  f' {DousFrame.decode_value(request_params)}')
+
+        if path in self.routes:
+            view = self.routes[path]
+        else:
+            view = PageNotFound404()
 
         for front in self.front_items:
             front(request)
@@ -25,3 +46,12 @@ class DousFrame:
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for key, value in data.items():
+            val = bytes(value.replace('%', '=').replace("+", " "), 'UTF-8')
+            value_string = decodestring(val).decode('UTF-8')
+            new_data[key] = value_string
+        return new_data
